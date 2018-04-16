881548d813a728dd954b806df7bf2fe60a960296
cahillalan <32340312+cahillalan@users.noreply.github.com>
2018-04-16 05:15:15 +0100

    Add files via upload
    Added the Menu Item Form and changed the def Menu View Function

--- forms.py
+++ forms.py
@@ -1,6 +1,52 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.db import transaction
+from appitems.models import Menu
+from django.forms import ModelForm
 
-class SignInForm(UserCreationForm):
+from accounts.models import Customer, User, Restaurant
+
+class CustomerSignUpForm(UserCreationForm):
+    address = forms.CharField(max_length=254, required=True)
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username','password1')
+        fields = ('username', 'email', 'password1', 'password2', 'address')
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_customer = True
+        user.save()
+        customer = Customer.objects.create(user=user)
+        customer.address = self.cleaned_data.get('address')
+        customer.save()
+        return user
+
+class RestaurantSignUpForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'password1', 'password2','first_name')
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_restaurant = True
+        user.save()
+        rest = Restaurant.objects.create(user=user, menu = Menu.objects.create())
+        rest.save()
+
+        return user
+
+class DriverSignUpForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_driver = True
+        user.save()
+        driver = Driver.objects.create(user=user)
+        return user
--- views.py
+++ views.py
@@ -1,81 +1,46 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.views.generic import CreateView
+from django.contrib.auth import login as auth_login
+
+from .forms import CustomerSignUpForm,RestaurantSignUpForm,DriverSignUpForm
+from .models import User
 from django.http import HttpResponse
-from accounts.models import Customer,Restaurant
-from appitems.models import Menu, MenuItem
-from django.views.generic import UpdateView, ListView
-from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import resolve, reverse
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import AbstractUser
-from django.http import Http404
+from .models import Customer
 
 
-
-def home(request):
-    restaurants = Restaurant.objects.all()
-    rest_names = list()
-    menu = Menu.objects.all()
-    menu_list= list()
-
-    for men in menu:
-        menu_list.append(men.restaurant)
-
-    for rest in restaurants:
-        rest_names.append(str(rest.user.id))
-
-    print(rest_names)
-
-    response_html = '<br>'.join(rest_names)
-
-    return HttpResponse(response_html)
-
-class MenuListView(ListView):
-    model = Restaurant
-    context_object_name = 'restaurants'
-    template_name = 'home.html'
-
-
-def MenuView(request, pk):
-
-    menuitem = list()
-    item = MenuItem.objects.all()
-    for i in item:
-        if i.id is 1:
-            menuitem.append(i)
-
-    return render(request, 'menu.html', {'menu': menuitem})
-
-@login_required
-def new_menu(request, pk):
-    restaurant = get_object_or_404(Restaurant, pk=pk)
+def customer_signup(request):
     if request.method == 'POST':
-        form = NewMenuForm(request.POST)
+        form = CustomerSignUpForm(request.POST)
         if form.is_valid():
-            menu = form.save(commit=False)
-            menu.restaurant = restaurant
-            menu.save()
-            return redirect('MenuListView')
+            user = form.save()
+            auth_login(request, user)
+        return redirect('home')
     else:
-        form = NewMenuForm()
-    return render(request, 'new_topic.html', {'board': board, 'form': form})
+        form = CustomerSignUpForm()
+    return render(request, 'customer_signup.html', {'form': form})
 
-
-def login(request):
+def restaurant_signup(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
+        form = RestaurantSignUpForm(request.POST)
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(request, username=username, password=password)
-
-            if user is not None:
-                auth_login(request, user)
-                return redirect('home')
-            else:
-                form = LoginForm()
-                return render(request, 'login.html', {'form': form})
-
+            restaurant = form.save()
+            auth_login(request, restaurant)
+        return redirect('home')
     else:
-        form = LoginForm()
-        return render(request, 'login.html', {'form': form})
+        form = RestaurantSignUpForm()
+    return render(request, 'restaurant_signup.html', {'form': form})
+
+def driver_signup(request):
+    if request.method == 'POST':
+        form = DriverSignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+        return redirect('home')
+    else:
+        form = DriverSignUpForm()
+    return render(request, 'driver_signup.html', {'form': form})
+
